ac2d64359ba5c71cc2bf4ea8deec5dde260b4948
Polycarpes-james <jamespolycarpes@gmail.com>
2026-07-09 19:26:54 +0100

    Gerer la partie administration proffesionnelle

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,8 +6,12 @@ namespace Database\Seeders;
 
 use App\Models\Avis;
 use App\Models\Category;
+use App\Models\Commande;
 use App\Models\Ingredient;
+use App\Models\Livraison;
 use App\Models\Menu;
+use App\Models\Panier;
+use App\Models\PanierPlat;
 use App\Models\Plat;
 use App\Models\Reservation;
 use App\Models\User;
@@ -25,12 +29,14 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RolePermissionSeeder::class,
         ]);
+        // User::factory(30)->create(); 
         $avis = Avis::factory(100)->create();
-        User::factory(30)->hasAttached($avis->random(15))->create(); 
-        Category::factory(10)->create();
+        // Category::factory(10)->create();
+
         $ingredients = Ingredient::factory(40)->create();
+
         $plats = Plat::factory(40)->hasAttached($ingredients->random(10), $avis->random(15))->create();
-        Menu::factory(10)->hasAttached($plats->random(10))->create();
-        Reservation::factory(20)->create();
+        // Menu::factory(10)->hasAttached($plats->random(10))->create();
+        // Reservation::factory(20)->create();
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @section('firstpart')
      <div class="main-header-container">
         <div class="presentation-part">
-            <h2>Degustez, Commandez Explosez</h2>
+            <h2>Degustez, Commandez Explosez 069339097</h2>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex alias in cum, repellendus sapiente facere eaque amet, delectus fugiat praesentium aliquid quasi consequatur perspiciatis neque quaerat necessitatibus explicabo velit labore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptate reiciendis sed optio tempore animi quasi, ex minus odio, ut, soluta commodi dolore a corrupti autem. Quas reiciendis provident itaque?  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, sunt odio reprehenderit deserunt autem repudiandae ullam quibusdam assumenda necessitatibus, voluptatem inventore nemo perspiciatis eos tempora beatae magnam rerum dolorum sint?
             </p>
